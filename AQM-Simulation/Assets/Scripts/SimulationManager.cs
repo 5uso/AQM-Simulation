4579e9f2872b7dadd6823f4fc1f1d9b63ebb4380
Suso <jesusmosteiro@gmail.com>
2023-10-08 20:17:04 +0200

    Visualization

--- AQM-Simulation/Assets/Scripts/SimulationManager.cs
+++ AQM-Simulation/Assets/Scripts/SimulationManager.cs
@@ -1,16 +1,20 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SimulationManager : MonoBehaviour {
-    TestSetup[] setups = new TestSetup[4];
+    TestSetup[] setups;
+
+    [SerializeField] private TMP_Text text;
+    [SerializeField] private TMP_Text[] sliderText;
 
     bool active = true;
 
-    Func<double, double, double, (bool, bool)> filter = Filters.qm_filter;
-    double a  =  0.0;
-    double a_ = 45.0;
+    double a  = 0;
+    double a_ = 45;
     double b  = 22.5;
     double b_ = 67.5;
 
@@ -18,15 +22,15 @@ public class SimulationManager : MonoBehaviour {
         return setups[0].estimate() - setups[1].estimate() + setups[2].estimate() + setups[3].estimate();
     }
 
-    // Start is called before the first frame update
     void Start() {
-        setups[0] = new TestSetup(a , b , filter);
-        setups[1] = new TestSetup(a , b_, filter);
-        setups[2] = new TestSetup(a_, b , filter);
-        setups[3] = new TestSetup(a_, b_, filter);
+        setups = transform.GetComponentsInChildren<TestSetup>();
+
+        System.Globalization.CultureInfo culture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
+        culture.NumberFormat.NumberDecimalSeparator = ".";
+
+        System.Threading.Thread.CurrentThread.CurrentCulture = culture;
     }
 
-    // Update is called once per frame
     void Update() {
         if(!active) return;
 
@@ -34,5 +38,42 @@ public class SimulationManager : MonoBehaviour {
             double polarization = UnityEngine.Random.value * 360.0;
             setup.photon(polarization);
         }
+
+        double s = estimate();
+        text.color = s <= 2 ? new Color32(0, 255, 0, 255) : new Color32(255, 0, 0, 255);
+        if (double.IsNaN(s)) s = 0.0;
+        text.text = string.Format("<mspace=0.6em>|S| = {0:0.0000} ≤ 2</mspace>", s);
+    }
+
+    public void updateA(float value)
+    {
+        a = value / 2.0;
+        setups[0].alpha = a;
+        setups[1].alpha = a;
+        sliderText[0].text = string.Format("{0:0.0}º", a);
+    }
+
+    public void updateA_(float value)
+    {
+        a_ = value / 2.0;
+        setups[2].alpha = a_;
+        setups[3].alpha = a_;
+        sliderText[1].text = string.Format("{0:0.0}º", a_);
+    }
+
+    public void updateB(float value)
+    {
+        b = value / 2.0;
+        setups[0].beta = b;
+        setups[2].beta = b;
+        sliderText[2].text = string.Format("{0:0.0}º", b);
+    }
+
+    public void updateB_(float value)
+    {
+        b_ = value / 2.0;
+        setups[1].beta = b_;
+        setups[3].beta = b_;
+        sliderText[3].text = string.Format("{0:0.0}º", b_);
     }
 }
--- AQM-Simulation/Assets/Scripts/TestSetup.cs
+++ AQM-Simulation/Assets/Scripts/TestSetup.cs
@@ -1,32 +1,87 @@
-using System;
+﻿using System;
+using TMPro;
 using UnityEngine;
 
-public class TestSetup {
+public class TestSetup : MonoBehaviour {
     public double alpha;
     public double beta;
 
-    Func<double, double, double, (bool, bool)> filter;
+    [SerializeField] private TMP_Text text;
+    [SerializeField] private TMP_Text sourceText;
+    [SerializeField] private TMP_Text[] splitterText;
+    [SerializeField] private TMP_Text[] detectorText;
 
+    [SerializeField] private GameObject[] beams;
+    private Material[] beamMats;
+
+    Func<double, double, double, (bool, bool)> filter = Filters.qm_filter;
+
+    private long total = 0;
     private long PP = 0;
     private long PN = 0;
     private long NP = 0;
     private long NN = 0;
-
-    public TestSetup(double alpha, double beta, Func<double, double, double, (bool, bool)> filter) {
-        this.alpha = alpha;
-        this.beta = beta;
-        this.filter = filter;
-    }
+    private long LP = 0;
+    private long LN = 0;
+    private long RP = 0;
+    private long RN = 0;
 
     public void photon(double polarization) {
+        total++;
         (bool passes_a, bool passes_b) r = filter(alpha, beta, polarization);
-        if( r.passes_a &&  r.passes_b) PP++;
-        if( r.passes_a && !r.passes_b) PN++;
-        if(!r.passes_a &&  r.passes_b) NP++;
-        if(!r.passes_a && !r.passes_b) NN++;
+        if(r.passes_a)
+        {
+            LP++;
+            if (r.passes_b) { RP++; PP++; }
+            else { RN++; PN++; }
+        }
+        else
+        {
+            LN++;
+            if (r.passes_b) { RP++; NP++; }
+            else { RN++; NN++; }
+        }
     }
 
     public double estimate() {
         return (double)(PP - PN - NP + NN) / (double)(PP + PN + NP + NN);
+    }
+
+    private void Start()
+    {
+        beamMats = new Material[beams.Length];
+        for (int i = 0; i < beams.Length; i++)
+        {
+            beamMats[i] = beams[i].GetComponent<Renderer>().material;
+            beamMats[i].SetFloat("_Intensity", 1.0f);
+        }
+    }
+
+    void Update()
+    {
+        double sum = PP + PN + NP + NN;
+        double a = PP / sum;
+        double b = PN / sum;
+        double c = NP / sum;
+        double d = NN / sum;
+        text.text = string.Format("<mspace=0.5em><#00DDFF>↑<#F000FF>↑<#FFFFFF> {0:0.00}   <#00DDFF>↑<#F1FF00>↓<#FFFFFF> {1:0.00}\r\n<#FF4040>↓<#F000FF>↑<#FFFFFF> {2:0.00}   <#FF4040>↓<#F1FF00>↓<#FFFFFF> {3:0.00}\r\n\r\n{4:0.0000}", a, b, c, d, estimate());
+
+        sourceText.text = string.Format("{0}", total);
+
+        double lpp = LP / sum;
+        double lnp = LN / sum;
+        double rpp = RN / sum;
+        double rnp = RN / sum;
+        beamMats[2].SetFloat("_Intensity", (float)lpp);
+        beamMats[3].SetFloat("_Intensity", (float)lnp);
+        beamMats[4].SetFloat("_Intensity", (float)rpp);
+        beamMats[5].SetFloat("_Intensity", (float)rnp);
+        detectorText[0].text = string.Format("{0:0.00}", lpp);
+        detectorText[1].text = string.Format("{0:0.00}", lnp);
+        detectorText[2].text = string.Format("{0:0.00}", rpp);
+        detectorText[3].text = string.Format("{0:0.00}", rnp);
+
+        splitterText[0].text = string.Format("{0:0.0}º", alpha);
+        splitterText[1].text = string.Format("{0:0.0}º", beta);
     }
 }
